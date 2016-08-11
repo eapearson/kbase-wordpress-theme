74bbc4fdc3973f321d9ebdeb3b18496f068b9932
Erik Pearson <eapearson@lbl.gov>
2016-08-10 21:21:32 -0700

    template for new home page to be able to invoke the specific style sheet

---
+++ template-home-page-2016.php
@@ -0,0 +1,24 @@
+<?php /**
+ * A page theme which is just like the home page. For mocking up new home pages, or for using on the home page. 
+ * 
+ * @package: @kbase 
+ * Template Name: Home 2016
+ *
+ */ ?>
+<?php wp_enqueue_style('home-page-2016'); ?>
+<?php get_header() ?>
+
+<div class="container-fluid home-page" style="max-width: 1024px; margin: 0 auto;">  
+    <div class="row">
+        <div class="col-md-12">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+                <?php endwhile;
+            else : ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
+</div> 
+
+<?php get_footer() ?>
